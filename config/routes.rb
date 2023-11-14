0f112267dfec4f4669eb364d25ddb7901f451b96
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-11-14 11:46:16 +0200

    full crud list refactored

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # CREATE ACTION
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # READ ACTION (ALL)
+  # get 'restaurants', to: 'restaurants#index'
+  # # READ ACTION (ONE)
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # UPDATE ACTION
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # DELETE ACTION
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
+  # resources :restaurants, only: [:index, :show]
+  # resources :restaurants, except [:index, :show]
+
 end
